14beddec4b52a3a33bb89a3ea87716f239eb831b
Nelson Wu <Nelsonwu820@gmail.com>
2023-03-29 10:11:58 -0400

    Adding a basic input form to submit tags

---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+
+const Input = () => {
+  return (
+    <section className="form-section">
+        <form className="form">
+            <input className="search-form" type="text" placeholder="Input a Possible Tag Like Dog, Cat, Flowers, etc"/>
+            <input className="btn" type="submit" value="Search"/>
+        </form>
+    </section>
+  )
+}
+
+export default Input

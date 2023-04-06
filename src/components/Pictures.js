410a9565e5d02b193c91108201193f6c19250b8b
Nelson Wu <Nelsonwu820@gmail.com>
2023-04-06 10:28:20 -0400

    Adding the env to my img src to serve static files

--- src/components/Pictures.js
+++ src/components/Pictures.js
@@ -1,9 +1,10 @@
 const Pictures = ({ tags }) => {
-  const cell = tags.map((img, index) => <img src={String(img)} alt="with the tags" className="pictures" key={index}></img>)
+  const cell = tags.map((img, index) => <img src={String(process.env.PUBLIC_URL + img)} alt="with the tags" className="pictures" key={index}></img>)
   
   return (
     <section>
         <div className="pictures-section">{cell}</div>
+
     </section>
   )
 }
